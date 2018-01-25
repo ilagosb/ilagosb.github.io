df9c9b88661dd1054f183c6b37463d36b197cb86
ilagosb <33226671+ilagosb@users.noreply.github.com>
2018-01-25 22:18:43 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slider').slick({
+  	  	autoplay: true,
+  	  	arrows:false,
+  	  	dots: true,
+        fade:true,
+  	  	infinite: true,
+  	  	autoplaySpeed:4500,
+  		speed: 1000,
+  		cssEase: 'ease-in'
+  });
+});
